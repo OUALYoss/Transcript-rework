483a517d09e34e55feb9b8886cac3b3fabae4cca
OUALY Ossama <ossamaoualy@gmail.com>
2025-12-30 14:21:21 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from transcript-rework!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/ingest_normla/ingestion.py
@@ -0,0 +1,28 @@
+import json 
+import os
+
+
+def load_json(file_path):
+    if not os.path.exists(file_path):
+        return None
+  
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        return data
+    
+def normalize_data(data):
+    #tri par start_time
+    data['messages'] = sorted(data['messages'], key=lambda x:x['start_time'])
+    
+    for message in data['messages']:
+        # Enlève les espaces multiples
+        message['content'] = ' '.join(message['content'].split())
+    
+    return data    
+
+
+if __name__ == "__main__":
+    ile_path = 'src/data/raw/t1_recrutement.json'
+    transcript_data = load_json(file_path)
+# Normaliser les données
+    normalized_data = normalize_data(transcript_data)
