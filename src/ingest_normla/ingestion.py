3662727a8c068a73fa358692586ec37def9e7752
OUALY Ossama <ossamaoualy@gmail.com>
2025-12-30 14:23:19 +0100

    first commit

--- src/ingest_normla/ingestion.py
+++ src/ingest_normla/ingestion.py
@@ -1,28 +1,29 @@
-import json 
+import json
 import os
 
 
 def load_json(file_path):
     if not os.path.exists(file_path):
         return None
-  
-    with open(file_path, 'r', encoding='utf-8') as f:
+
+    with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
         return data
-    
+
+
 def normalize_data(data):
-    #tri par start_time
-    data['messages'] = sorted(data['messages'], key=lambda x:x['start_time'])
-    
-    for message in data['messages']:
+    # tri par start_time
+    data["messages"] = sorted(data["messages"], key=lambda x: x["start_time"])
+
+    for message in data["messages"]:
         # Enlève les espaces multiples
-        message['content'] = ' '.join(message['content'].split())
-    
-    return data    
+        message["content"] = " ".join(message["content"].split())
+
+    return data
 
 
 if __name__ == "__main__":
-    ile_path = 'src/data/raw/t1_recrutement.json'
+    file_path = "src/data/raw/t1_recrutement.json"
     transcript_data = load_json(file_path)
-# Normaliser les données
+    # Normaliser les données
     normalized_data = normalize_data(transcript_data)
